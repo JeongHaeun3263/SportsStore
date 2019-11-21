7c896d186414189847520244292dc645577b0e38
HAEUN JEONG <jhe3263@gmail.com>
2019-11-20 23:32:08 -0500

    Completed the Cart, Orders

--- SportsStore/Models/CartLine.cs
+++ SportsStore/Models/CartLine.cs
@@ -7,7 +7,7 @@ namespace SportsStore.Models
 {
     public class CartLine
     {
-        public int CartLindID { get; set; }
+        public int CartLineID { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
     }
